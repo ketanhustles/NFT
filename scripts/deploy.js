9217867d113aefd10a4b6ca1a36fd2a6ce89544d
ketanhustles <ketanhustles@gmail.com>
2024-09-16 19:58:07 +0530

    initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+//0xb439304f0C317e34E0439ce3C53C54ee81412575 
---
+++ scripts/mint-nft.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const API_URL = process.env.API_URL;
+
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+
+console.log(JSON.stringify(contract.abi));
